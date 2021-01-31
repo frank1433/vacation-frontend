c1d4476cdf2035cc15d47169509ac966ff704c87
frank zhang <yfrankzhang@yahoo.com>
2021-01-31 15:50:55 -0500

    update the warning prt

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,21 +12,26 @@ const Navbar = () => {
   const location = useLocation();
   const history = useHistory();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
-  useEffect(() => {
-    const token = user?.token;
-    if (token) {
-      const decodedToken = decode(token);
-      if (decodedToken.exp * 1000 < new Date().getTime()) {
-        logout();
-      }
-    }
-    setUser(JSON.parse(localStorage.getItem("profile")));
-  }, [location]);
+
   const logout = () => {
     dispatch({ type: "LOGOUT" });
     history.push("/");
     setUser(null);
   };
+  useEffect(
+    () => {
+      const token = user?.token;
+      if (token) {
+        const decodedToken = decode(token);
+        if (decodedToken.exp * 1000 < new Date().getTime()) {
+          logout();
+        }
+      }
+      setUser(JSON.parse(localStorage.getItem("profile")));
+    }, // eslint-disable-next-line react-hooks/exhaustive-deps
+    [location]
+  ); // eslint-disable-next-line react-hooks/exhaustive-deps
+
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
       <div className={classes.brandContainer}>
